9002e56166e4e2a9b63b2b96c7f1c9b2008b187e
Shohrab Rustam <85831433+ShohrabRustam@users.noreply.github.com>
2022-03-28 15:22:12 +0530

    some more idea to decleare the variable in cpp

--- Array/reverseOfArray.cpp
+++ Array/reverseOfArray.cpp
@@ -1,32 +1,33 @@
 #include <algorithm>
 #include <iostream>
 using namespace std;
-  
+
 int main()
 {
     // Get the array
-    cout <<"Enter the Size of Array "<<endl;
+    cout << "Enter the Size of Array " << endl;
     int n;
-    cin >>n;
+    cin >> n;
     int arr[n];
-    cout <<"Enter the "<<n <<" element in array "<<endl;
-    for(int i=0;i<n;i++){
+    cout << "Enter the " << n << " element in array " << endl;
+    for (int i = 0; i < n; i++)
+    {
         // cout <<"Enter the "<<i<<"th element of array "<<endl;
-        cin >>arr[i];
+        cin >> arr[i];
     }
-  
+
     // Print the array
-    cout << "Original Array: "<<endl;
+    cout << "Original Array: " << endl;
     for (int i = 0; i < n; i++)
         cout << arr[i] << " ";
-  
+
     // Reverse the array
     reverse(arr, arr + n);
 
-  
     // Print the reversed array
-    cout << "\nReversed Array: "<<endl;
+    cout << "\nReversed Array: " << endl;
     for (int i = 0; i < n; i++)
         cout << arr[i] << " ";
+
     return 0;
 }
---
+++ Some Knowledege/somethingnew.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a {2};
+    int b {3};
+    cout <<"The value of a : "<<a<<endl;
+    cout <<"The value of b : "<<b<<endl;
+return 0;
+}
