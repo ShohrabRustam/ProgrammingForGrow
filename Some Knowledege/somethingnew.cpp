7335dccd546f67fe8fda68fdcec4754e07f527c2
Shohrab Rustam <85831433+ShohrabRustam@users.noreply.github.com>
2022-03-28 17:35:02 +0530

    initialize using braces and functional

--- Some Knowledege/somethingnew.cpp
+++ Some Knowledege/somethingnew.cpp
@@ -6,8 +6,14 @@ int main()
     int a {2};
     int b {3};
     float c{2.4};
-    cout <<"The value of a : "<<a<<endl;
+    cout <<"The value of a : using Braces : "<<a<<endl;
     cout <<"The value of b : "<<b<<endl;
     cout<<"The value of c : "<<c<<endl;
+
+    //Functional Initializer 
+    int a1(1);
+    int b1(3);
+    cout <<"The value of a1 using functional Initialization : "<<a1<<endl;
+    cout <<"The value of b1 : using functional Initialization : "<<b1<<endl;
 return 0;
 }
