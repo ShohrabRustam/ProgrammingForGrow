f89df6b8d58ba75dcf5f4042ae8ad1deb67a7d91
Shohrab Rustam <85831433+ShohrabRustam@users.noreply.github.com>
2022-03-28 12:31:13 +0530

    reverse array

--- Array/reverseOfArray.cpp
+++ Array/reverseOfArray.cpp
@@ -5,23 +5,27 @@ using namespace std;
 int main()
 {
     // Get the array
+    cout <<"Enter the Size of Array "<<endl;
     int n;
     cin >>n;
     int arr[n];
+    cout <<"Enter the "<<n <<" element in array "<<endl;
     for(int i=0;i<n;i++){
+        // cout <<"Enter the "<<i<<"th element of array "<<endl;
         cin >>arr[i];
     }
   
     // Print the array
-    cout << "Array: ";
+    cout << "Original Array: "<<endl;
     for (int i = 0; i < n; i++)
         cout << arr[i] << " ";
   
     // Reverse the array
     reverse(arr, arr + n);
+
   
     // Print the reversed array
-    cout << "\nReversed Array: ";
+    cout << "\nReversed Array: "<<endl;
     for (int i = 0; i < n; i++)
         cout << arr[i] << " ";
     return 0;
