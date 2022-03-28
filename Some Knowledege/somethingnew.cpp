7817c80033c3337072c21bbfda57bb94399a1513
Shohrab Rustam <85831433+ShohrabRustam@users.noreply.github.com>
2022-03-28 17:38:37 +0530

    Notation Initialization

--- Some Knowledege/somethingnew.cpp
+++ Some Knowledege/somethingnew.cpp
@@ -3,17 +3,25 @@ using namespace std;
 int main()
 {
     // Braces initializer_list
-    int a {2};
-    int b {3};
+    int a{2};
+    int b{3};
     float c{2.4};
-    cout <<"The value of a : using Braces : "<<a<<endl;
-    cout <<"The value of b : "<<b<<endl;
-    cout<<"The value of c : "<<c<<endl;
+    cout << "The value of a : using Braces : " << a << endl;
+    cout << "The value of b : using Braces : " << b << endl;
+    cout << "The value of c : using Braces : " << c << endl;
 
-    //Functional Initializer 
+    // Functional Initializer
     int a1(1);
     int b1(3);
-    cout <<"The value of a1 using functional Initialization : "<<a1<<endl;
-    cout <<"The value of b1 : using functional Initialization : "<<b1<<endl;
-return 0;
+    cout << "The value of a1 using functional Initialization : " << a1 << endl;
+    cout << "The value of b1 : using functional Initialization : " << b1 << endl;
+
+    // assignment notation
+    int var1 = 7;
+    int var2 = 6;
+
+    cout << "The value of var1 using the assignment notation Initialization is : " << var1 << endl;
+    cout << "The value of var2 using the assignment notation Initialization is : " << var2 << endl;
+
+    return 0;
 }
