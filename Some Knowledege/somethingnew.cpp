a303a32f5b130febc28fe31efe26bf938a8d5a71
Shohrab Rustam <85831433+ShohrabRustam@users.noreply.github.com>
2022-03-28 17:27:06 +0530

    Braces Initializer

--- Some Knowledege/somethingnew.cpp
+++ Some Knowledege/somethingnew.cpp
@@ -2,9 +2,12 @@
 using namespace std;
 int main()
 {
+    // Braces initializer_list
     int a {2};
     int b {3};
+    float c{2.4};
     cout <<"The value of a : "<<a<<endl;
     cout <<"The value of b : "<<b<<endl;
+    cout<<"The value of c : "<<c<<endl;
 return 0;
 }
