5b53d7be08ce4f6b3384d7a69e81c95d2aa280ab
Shohrab Rustam <85831433+ShohrabRustam@users.noreply.github.com>
2022-04-11 14:33:28 +0530

    inversion of the element by brote froce

---
+++ Algo/fincinversion.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<vector>
+#include<vector>
+using namespace std;
+int inversion(int a[],int n){
+    int count = 0;
+    for(int i=0;i<n-1;i++){
+        for(int j = i+1;j<n;j++){
+            if(a[i]>a[j] && i<j){
+                count++;
+            }
+        }
+    }
+    return count;
+}
+int main (){
+    int n ;
+    // size of array 
+    cin >>n;
+    int a[n];
+    for(int i=0;i<n;i++){
+        cin >>a[i];
+    }
+    int result = inversion(a,n);
+
+    cout<<endl<<"Result : "<<result<<endl;
+}
