f62dbb3289ec2b5f57b58b192bac3dbd85c96645
Shohrab Rustam <85831433+ShohrabRustam@users.noreply.github.com>
2022-05-19 18:32:30 +0530

    setup the std library

--- Basic/std.cpp
+++ Basic/std.cpp
@@ -2,7 +2,9 @@
 int main(){
 std::cout << "Hello, world!" << std::endl;
  // write your code here  
- std::cout <<"My name is Shohrab Rustam "<<endl;
+ std::cout <<"My name is Shohrab Rustam "<<std::endl;
+  std::cout <<"My name is Shohrab Rustam "<<std::endl;
+
 //  system("pause>0");
 return 0;
 }
