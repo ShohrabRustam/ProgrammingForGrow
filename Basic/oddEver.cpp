efc6ae6c2ff3a7bbb03ba4f7850f4dfdccd784f7
Shohrab Rustam <85831433+ShohrabRustam@users.noreply.github.com>
2022-05-19 19:06:50 +0530

    odd even

---
+++ Basic/oddEver.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+int a=1000;
+if(a%2==0){
+    cout <<"Number is Even "<<endl;
+}else{
+cout <<"Number is Odd "<<endl;
+}
+ // write your code here  
+return 0;
+}
