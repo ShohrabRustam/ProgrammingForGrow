989018602f4a933b1f43113af3ff82d746b60a4b
Shohrab Rustam <85831433+ShohrabRustam@users.noreply.github.com>
2022-05-19 19:04:41 +0530

    char

--- Basic/size.cpp
+++ Basic/size.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include <bits/stdc++.h>
 using namespace std;
 int main(){
 int i = 0;
@@ -10,6 +11,8 @@ cout <<"Size of int is : "<<sizeof(i)<<endl;
 cout <<"Size of float is : "<<sizeof(j)<<endl;;
 cout <<"Size of char is : "<<sizeof(a)<<endl;
 cout <<"Size of string is : "<<sizeof(s)<<endl;
+cout <<"Size of INT MIN is " <<INT_MIN<<endl;
+cout <<"Size of INT MIN is " <<INT_MAX<<endl;
 
 return 0;
 }
