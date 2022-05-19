5458f398cef8292e228a173834aa40dd164567af
Shohrab Rustam <85831433+ShohrabRustam@users.noreply.github.com>
2022-05-19 18:37:30 +0530

    namespace

--- Basic/usingnamespace.cpp
+++ Basic/usingnamespace.cpp
@@ -1,7 +1,9 @@
 #include<iostream>
 using namespace std;
 int main(){
+    int number=0;
 cout <<"Hello, world!" <<endl;
  // write your code here  
+ cout <<"The number is :" <<number<<endl;
 return 0;
 }
