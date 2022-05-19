794c6b59638c500f8809a0fc06ac68fdce58cc07
Shohrab Rustam <85831433+ShohrabRustam@users.noreply.github.com>
2022-05-19 18:51:02 +0530

    size of data type

---
+++ Basic/size.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+int i = 0;
+float j= 9.6;
+char a='a';
+string s= "hello";
+
+cout <<"Size of int "<<sizeof(i)<<endl;
+cout <<"Size of float "<<sizeof(j)<<endl;;
+cout <<"Size of char"<<sizeof(a)<<endl;
+cout <<"Size of string "<<sizeof(s)<<endl;
+
+return 0;
+}
