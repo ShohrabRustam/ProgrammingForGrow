edcfeb3afe8b74e4d044597bb33cf5e6e822ba85
Shohrab Rustam <85831433+ShohrabRustam@users.noreply.github.com>
2022-05-20 18:45:20 +0530

    digits

--- Basic/numberOfDigit.cpp
+++ Basic/numberOfDigit.cpp
@@ -1,17 +1,25 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
-int num;
-cout <<"Enter the number to count Number of Digits " << endl;
-cin >> num;
-int count=0;
-while (num)
+int main()
 {
-    count++;
-    num=num/10;
-}
-cout <<"The number of Digits is " << count << endl;
+    int num;
+    cout << "Enter the number to count Number of Digits " << endl;
+    cin >> num;
+    int count = 0;
+    if (num < 0)
+    {
+        num = (-1) * num;
+    }
+    if (num == 0)
+    {
+        cout << "Number is " << num << "Digists is : " << 1 << endl;
+    }
+    while (num)
+    {
+        count++;
+        num = num / 10;
+    }
+    cout << "The number of Digits is " << count << endl;
 
-
-return 0;
+    return 0;
 }
