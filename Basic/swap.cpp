0bdba5dd7fddd374c7eb3af5a5d13153984f6498
Shohrab Rustam <85831433+ShohrabRustam@users.noreply.github.com>
2022-05-20 13:50:50 +0530

    swap the varible using without third variable

---
+++ Basic/swap.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+int a=12;
+int b=21;
+int c;
+cout<<"Value of a and b are before swapping : " << a<<"  " <<b<<endl;
+ // write your code here  
+ c= a;
+ a= b;
+ b= c;
+ cout<<"Value of a and b are before swapping : " <<a<<"  " <<b<<endl;
+
+return 0;
+}
