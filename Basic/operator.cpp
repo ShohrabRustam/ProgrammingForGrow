ca2e6b392fb5c8689b184f24b31e7fba10054c32
Shohrab Rustam <85831433+ShohrabRustam@users.noreply.github.com>
2022-05-20 11:53:09 +0530

    some basic operator in cpp

---
+++ Basic/operator.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+// operator +,-,/,*,%
+int a;
+int b;
+cout <<"Enter the first number "<<endl;
+cin >> a;
+cout <<"Enter the second number "<<endl;
+cin >> b;
+
+cout <<"Sum of the numbers is : "<<a+b<<endl;
+cout <<"Subtract the numbers is : "<<a-b<<endl;
+cout <<"Divide the numbers is : "<<(float)a/b<<endl;
+cout <<"Multiply the numbers is : "<<a*b<<endl;
+cout <<"Modulo the numbers is : "<<a%b<<endl;
+
+return 0;
+}
