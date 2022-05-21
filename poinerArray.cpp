84b6b2b3d7d6892a3985bb7d91668970ebae9696
Shohrab Rustam <85831433+ShohrabRustam@users.noreply.github.com>
2022-05-21 17:26:22 +0530

    pointer example using in array

--- poinerArray.cpp
+++ poinerArray.cpp
@@ -2,13 +2,30 @@
 using namespace std;
 int main()
 {
-    int luckyNumber[] = {2, 3, 5, 7, 9, 13};
-    cout << luckyNumber << endl;
-    cout << &luckyNumber[1] << endl;
+    // int luckyNumber[5] = {2, 3, 5, 7, 9, 13};
+    // cout << luckyNumber << endl;
+    // cout << &luckyNumber[1] << endl;
 
-    cout << luckyNumber[2] << endl;
+    // cout << luckyNumber[2] << endl;
 
-    cout << *(luckyNumber+2) << endl;
+    // cout << *(luckyNumber+2) << endl;
+    int luckyNumber[5];
 
+    for (int i = 0; i <= 5; i++)
+    {
+        cout << "Enter " << i << " Number : " << endl;
+        cin >> luckyNumber[i];
+    }
+
+    cout <<endl<< "Normal OUTPUT : " << endl;
+    for (int i = 0; i <= 5; i++)
+    {
+        cout << luckyNumber[i] << endl;
+    }
+     cout <<endl<< "Pointer OUTPUT : " << endl;
+    for (int i = 0; i <= 5; i++)
+    {
+        cout << *(luckyNumber + i) << endl;
+    }
     return 0;
 }
