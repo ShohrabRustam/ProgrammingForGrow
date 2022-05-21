abf07d582fa7288a9994b477ae9bb2a92726af98
Shohrab Rustam <85831433+ShohrabRustam@users.noreply.github.com>
2022-05-21 12:33:21 +0530

    obj and class

--- OOPS/classObj.cpp
+++ OOPS/classObj.cpp
@@ -14,7 +14,7 @@ public:
         cout << "Organization orgName : " << Organization::orgName << endl;
         cout << "Organization CEO :" << Organization::ceoName << endl;
         cout << "Organization Description : " << Organization::desc << endl;
-        cout << "Organization numberOfEmployees : "<<Organization::numbersOfEmployees<<endl;
+        cout << "Organization numberOfEmployees : " << Organization::numbersOfEmployees << endl;
     }
 };
 int main()
@@ -23,13 +23,27 @@ int main()
     object.orgName = "Mpokket";
     object.ceoName = "Gourav Jalan";
     object.desc = "The Best Organization in fintec";
-    object.numbersOfEmployees =1269;
-    object.bestEmployees ={"Rustam","Salma","Hanvesh","Abhishek"};
+    object.numbersOfEmployees = 1269;
+    object.bestEmployees = {"Rustam", "Salma", "Hanvesh", "Abhishek"};
     object.aboutCompany();
-    cout << "Best Employee: " <<endl;
-    for (string employess: object.bestEmployees){
-        cout << employess <<"    ";
+    cout << "Best Employee: " << endl;
+    for (string employess : object.bestEmployees)
+    {
+        cout << employess << "    ";
     }
-    cout <<endl;
+cout << endl;
+    Organization object1;
+    object1.orgName = "Mpokket PVT";
+    object1.ceoName = "Ajay Jalan ";
+    object1.desc = "The Best Organization IN Fintec";
+    object1.numbersOfEmployees = 1700;
+    object1.bestEmployees = { "Umme Salma","Shohrab Rustam", "Hanvesh", "Abhishek "};
+    object1.aboutCompany();
+    for (string employess : object1.bestEmployees)
+    {
+        cout << employess << "    ";
+    }
+
+    cout << endl;
     return 0;
 }
