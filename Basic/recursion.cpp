a1724503023f7b4e37096ff5be0f052c9020e025
Shohrab Rustam <85831433+ShohrabRustam@users.noreply.github.com>
2022-05-21 11:27:08 +0530

    sum of between the numbers

---
+++ Basic/recursion.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+template<class T>
+int sum(T n, T m){
+    int sum = 0;
+    for(int i=n; i<=m; i++) 
+    sum +=i;
+    return sum;
+}
+int main(){
+//sum of between two numbers
+int n;
+cout << endl << "Recursive function to find the sum  between of two numbers" << endl;
+cout <<endl << "Enter the First numbers" << endl;
+cin >> n;
+int m ;
+cout << "Enter the First numbers" << endl;
+cin >> m;
+cout<<endl<<sum(n,m)<<endl;
+return 0;
+}
