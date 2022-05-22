73515718efc9f69021091d3c7841b6f4b484391b
Shohrab Rustam <85831433+ShohrabRustam@users.noreply.github.com>
2022-05-22 18:12:31 +0530

    pointer

---
+++ Arrays/DynamixMultiDiaArray.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int rows,cols;
+    cout << " Enter Rows and cols " <<endl;
+    cin>>rows>>cols;
+    
+    int** table= new int*[rows];
+    for (int i = 0; i <rows; i++){
+       table[i]=new int[cols];
+    }
+
+    for (int i = 0; i <rows; i++){
+         delete table[i];
+    }
+       delete table;
+       table = NULL;
+return 0;
+}
