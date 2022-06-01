6d4ab95a53f6ed9ca902dbb18b5b392e9402c620
Shohrab Rustam <85831433+ShohrabRustam@users.noreply.github.com>
2022-06-01 11:46:54 +0530

    setup random number generate

--- Basic/randomNumber.cpp
+++ Basic/randomNumber.cpp
@@ -1,9 +1,11 @@
-#include<iostream>
-#include<cstdlib>
+#include <iostream>
+#include <cstdlib>
 using namespace std;
-int main(){
+int main()
+{
     // to gerate the random number on time0
     srand(time(0));
-    cout <<random()%220 <<endl;
-return 0;
+
+    cout << random() % 6 << endl;
+    return 0;
 }
