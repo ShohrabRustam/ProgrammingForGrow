5de3c39887db05687785318961ab3db8ede88941
Shohrab Rustam <85831433+ShohrabRustam@users.noreply.github.com>
2022-06-01 13:14:52 +0530

    make a file in cpp with some text

--- Basic/file.cpp
+++ Basic/file.cpp
@@ -4,5 +4,10 @@ using namespace std;
 int main()
 {
 
+    // object of the filestream
+    ofstream ofsFile;
+    ofsFile.open("rustam.txt");
+    ofsFile << "I love Programming and programming Love me !" << endl;
+    ofsFile.close();
     return 0;
 }
