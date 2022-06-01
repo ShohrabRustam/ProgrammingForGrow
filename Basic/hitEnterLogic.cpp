177ed4c731e50f377dc946303bcbaa43ae4f81d6
Shohrab Rustam <85831433+ShohrabRustam@users.noreply.github.com>
2022-06-01 10:35:38 +0530

    hit enter logic in cpp

---
+++ Basic/hitEnterLogic.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+if (cin.get() == '\n'){
+    // Good to knwo that concept 
+    cout << "You Hit Enter now we can get the action !! "<<endl;
+}
+    return 0;
+}
