be8f818952bbf3386d1c76f8ce1391b23090835d
Shohrab Rustam <85831433+ShohrabRustam@users.noreply.github.com>
2022-06-01 12:05:32 +0530

    timestamp in c

--- Basic/randomNumber.cpp
+++ Basic/randomNumber.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 #include <cstdlib>
+#include <ctime>
 using namespace std;
 int main()
 {
     // to gerate the random number on time0
     srand(time(0));
+    cout <<time(0) << " ";
 
     cout << random() % 6 << endl;
     return 0;
